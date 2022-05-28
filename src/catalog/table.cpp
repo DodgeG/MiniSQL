ae69841fc1e4efcaf85cd132e19936e9831b6b32
gdz <13954899675@163.com>
2022-05-28 16:00:31 +0800

    test

--- src/catalog/table.cpp
+++ src/catalog/table.cpp
@@ -1,18 +1,49 @@
 #include "catalog/table.h"
 
 uint32_t TableMetadata::SerializeTo(char *buf) const {
-  return 0;
+  uint32_t ofs = sizeof(size_t);
+  MACH_WRITE_TO(uint32_t, buf, TABLE_METADATA_MAGIC_NUM);
+  MACH_WRITE_TO(table_id_t, buf+ofs, table_id_);
+  ofs+=sizeof(table_id_t);
+  MACH_WRITE_TO(size_t, buf+ofs, table_name_.size());
+  ofs+=sizeof(size_t);
+  MACH_WRITE_STRING(buf+ofs, table_name_);
+  ofs+=table_name_.size();
+  MACH_WRITE_TO(page_id_t,buf+ofs,root_page_id_);
+  ofs+=sizeof(page_id_t);
+  (*schema_).SerializeTo(buf+ofs);
+  ofs+=(*schema_).GetSerializedSize();
+  
+  return ofs;
 }
 
 uint32_t TableMetadata::GetSerializedSize() const {
-  return 0;
+  return sizeof(uint32_t)+sizeof(size_t)+table_name_.size()+sizeof(table_id_t)+sizeof(page_id_t)+(*schema_).GetSerializedSize();
 }
 
 /**
  * @param heap Memory heap passed by TableInfo
  */
 uint32_t TableMetadata::DeserializeFrom(char *buf, TableMetadata *&table_meta, MemHeap *heap) {
-  return 0;
+  uint32_t MAGIC_NUM = MACH_READ_FROM(uint32_t, buf);
+  ASSERT(TABLE_METADATA_MAGIC_NUM == MAGIC_NUM,"TABLE FORMAT ERROR!!");
+  uint32_t ofs = sizeof(table_id_t);
+  table_id_t table_id = MACH_READ_FROM(table_id_t, buf+ofs);
+  ofs += sizeof(table_id_t);
+  size_t size = MACH_READ_FROM(size_t, buf+ofs);
+  ofs+=sizeof(size_t);
+  char ch[size+1];
+  memcpy(ch, buf+ofs, size);
+  ch[size]='\0';
+  std::string table_name(ch);
+  ofs+=size;
+  page_id_t root_page_id = MACH_READ_FROM(page_id_t, buf+ofs);
+  ofs+=sizeof(page_id_t);
+  Schema *schema;
+  ofs+=Schema::DeserializeFrom(buf+ofs, schema, heap);
+  //
+  table_meta = ALLOC_P(heap,TableMetadata)(table_id, table_name, table_name, root_page_id, schema);
+  return ofs;
 }
 
 /**
